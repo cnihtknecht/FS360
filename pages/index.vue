1111cfeb0a5b9c45dc816efb0cda7404576c86c2
cnihtknecht <56153864+cnihtknecht@users.noreply.github.com>
2020-07-04 14:15:45 +0800

    tutorial1 end

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="flex h-screen w-full bg-gray-700 ">
     <div class="m-auto w-full px-5 md:w-1/3">
+      <div class="pl-1 text-white font-medium text-2xl tracking-wide text-center">
+        Inventory System {{ email }}  
+      </div>
     <form>
-      <inputElement 
-        :labelName="'EMAIL'"
-        :labelId="'email'"
-        :inputType="'email'"
-        :model="email"
-        @input="email = $event"
-        :disabled="false"
-        />
+      <div class="my-2">
+        <inputElement 
+          :labelName="'EMAIL'"
+          :labelId="'email'"
+          :inputType="'email'"
+          :model="email"
+          @input="email = $event"
+          :disabled="false"
+          />
+      </div>
+      <div class="my-2">
         <inputElement 
         :labelName="'PASSWORD'"
         :labelId="'password'"
@@ -18,7 +24,24 @@
         @input="password = $event"
         :disabled="false"
         />
-        <button type="button"> SIGN IN </button>
+      </div>
+        <button type="button" class="
+        transition 
+        duration-500 
+        ease-in-out 
+        bg-blue-600 
+        rounded-xs 
+        w-full
+        py-3 
+        text-white
+        font-sans 
+        font-bold
+        tracking-wide
+        hover:bg-blue-700
+        hover:text-gray-400
+        active:outline-none
+        focus:outline-none"> 
+          SIGN IN </button>
     </form>
     </div>
   </div>

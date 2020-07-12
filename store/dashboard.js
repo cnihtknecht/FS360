90e9f12fbe3729f6726e559f3f46f7544d0a771a
cnihtknecht <56153864+cnihtknecht@users.noreply.github.com>
2020-07-12 13:32:41 +0800

    Update dashboard.js

---
+++ store/dashboard.js
@@ -0,0 +1,15 @@
+const defaultState = () => {
+    return{
+        showMenu: false
+    }
+};
+
+export const state = () => defaultState();
+
+export const mutations = {
+    toggleMenu(state){
+        state.showMenu = !state.showMenu
+    }
+}
+
+
